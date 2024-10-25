5604f6d0be70cc8b9f286e5fe4ea2ceea538def0
BT_1N3_21 <sample@bths.edu>
2024-10-25 13:12:25 -0400

    More Stuff.2!!

--- src/Apple.java
+++ src/Apple.java
@@ -2,11 +2,17 @@ public class Apple {
     public static void main(String[] args) {
         System.out.println("Apple");
         System.out.println("Banana");
-        System.out.println("Chicken");
+        System.out.println("Burger");
         int number = 1;
+        int number2 = 0;
         while (number < 6) {
             System.out.println("Apple " + number);
             number++;
+            while (number2 < 4) {
+                System.out.println("Banana " + number2);
+                number2++;
+            }
+            number2 = 0;
         }
     }
 }
